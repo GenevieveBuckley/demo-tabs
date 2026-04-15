9113703dfa9b20f48f124c369a649b66bbc038f4
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2026-04-15 14:37:21 +1000

    Switch to using <Plot> html element

--- src/Tab1.tsx
+++ src/Tab1.tsx
@@ -1,5 +1,4 @@
-import Typography from "@mui/material/Typography";
-import Plotly from "plotly.js-dist-min";
+import Plot from "plotly.js-dist-min";
 
 export function Tab1() {
   const trace = {
@@ -12,31 +11,20 @@ export function Tab1() {
 
   const data = [trace];
 
-  const layout = {
-    title: "Simple Plotly Line Chart",
-    xaxis: {
-      title: "X Axis",
-    },
-    yaxis: {
-      title: "Y Axis",
-    },
-  };
-  Plotly.newPlot("chart", data, layout);
   return (
-    <div id="chart"></div>
-    // <Typography sx={{ marginBottom: 2 }}>
-    //   Tab One Content... Lorem ipsum dolor sit amet, consectetur adipiscing
-    //   elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    //   Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus
-    //   at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-    //   quisque non tellus. Convallis convallis tellus id interdum velit laoreet
-    //   id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-    //   Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus
-    //   euismod quis viverra nibh cras. Metus vulputate eu scelerisque felis
-    //   imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa
-    //   tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-    //   arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi
-    //   tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-    // </Typography>
+    <div className="Plot">
+      <Plot
+        data={data}
+        layout={{
+          title: "Simple Plotly Line Chart",
+          xaxis: {
+            title: "X Axis",
+          },
+          yaxis: {
+            title: "Y Axis",
+          },
+        }}
+      />
+    </div>
   );
 }
