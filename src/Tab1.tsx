6c7e09928b05b308f31e742180987dbb210cd98a
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2026-04-15 15:23:11 +1000

    Even more minimal plotly example (produces error)

--- src/Tab1.tsx
+++ src/Tab1.tsx
@@ -1,30 +1,9 @@
 import Plot from "plotly.js-dist-min";
 
 export function Tab1() {
-  const trace = {
-    x: [1, 2, 3, 4, 5],
-    y: [10, 15, 13, 17, 21],
-    mode: "lines+markers",
-    type: "scatter",
-    name: "Sample Data",
-  };
-
-  const data = [trace];
-
   return (
     <div className="Plot">
-      <Plot
-        data={data}
-        layout={{
-          title: "Simple Plotly Line Chart",
-          xaxis: {
-            title: "X Axis",
-          },
-          yaxis: {
-            title: "Y Axis",
-          },
-        }}
-      />
+      <Plot data={[{ x: [1, 2], y: [3, 4], type: "bar" }]} />
     </div>
   );
 }
